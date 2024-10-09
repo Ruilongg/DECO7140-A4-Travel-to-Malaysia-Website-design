507df77a93612f35d7f85fd27f50f8fdc8b5a1b3
Abc-47258369 <ruilong.huang@uqconnect.edu.au>
2024-10-09 10:09:50 +1000

    a4
    code review update

--- assessment/js/index.js
+++ assessment/js/index.js
@@ -1,6 +1,6 @@
-import { showPageLoadMessage } from './page_load_message.js';
+import { consoleMessage } from './page_load_message.js';
 
 console.log('Index page loaded');
 
 // Call the function from page_load_message.js to display a message
-showPageLoadMessage();
+consoleMessage();
--- assessment/js/page_load_message..js
+++ assessment/js/page_load_message..js
@@ -1,3 +1,6 @@
-export function showPageLoadMessage() {
+// Export the consoleMessage function so that it can be imported and used in other files (like index.js)
+export function consoleMessage() {
+    // Log a message in the console indicating that the page has successfully loaded
     console.log('Page has loaded successfully.');
 }
+
