6cf606181ad29dd906b20339de0610cc17c81f3a
Abc-47258369 <ruilong.huang@uqconnect.edu.au>
2024-10-16 01:07:59 +1000

    a4
    code update

--- assessment/js/index.js
+++ assessment/js/index.js
@@ -1,6 +1,14 @@
+// Import the consoleMessage function from the page_load_message.js file
 import { consoleMessage } from './page_load_message.js';
 
-console.log('Index page loaded');
+// Log a message in the console indicating that the index page has been loaded
 
-// Call the function from page_load_message.js to display a message
+
+// Call the consoleMessage function (imported from page_load_message.js)
+// This function will log "Page has loaded successfully." to the console
 consoleMessage();
+
+
+//display a confirmation message when the page loads, and it does so using a modular approach
+
+
